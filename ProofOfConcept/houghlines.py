5f07731bd55755525f16636731c6536cf08f7f04
Thomas Harris <teh019283@gmail.com>
2015-04-11 12:36:05 -0400

    Now finds the y indices of the shelves

--- ProofOfConcept/houghlines.py
+++ ProofOfConcept/houghlines.py
@@ -3,8 +3,20 @@ import cv2
 import numpy as np
 import sys
 
+def consolidate(ys, dv, img, color):
+    for i in range(len(ys) - 1):
+        if ys[i+1] - ys[i] < dv:
+            ys[i] = (ys[i] + ys[i+1]) /2
+            ys[i+1] = ys[i]
+        if not color is None:
+            cv2.line(img, (0,ys[i]), (img.shape[1],ys[i]), color, 2)
+
+def uniq (l):
+    l = list(set(l))
+    l.sort()
+    return l
+
 img = cv2.imread(sys.argv[1])
-img2 = img
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret,bw = cv2.threshold(gray, 200,230, cv2.THRESH_BINARY)
 edges = cv2.Canny(bw, 100, 200, apertureSize=3)
@@ -19,14 +31,16 @@ for x0,y0,x1,y1 in lines[0]:
     x1 = img.shape[1]
     if y0-y1 < 10 and y0-y1 > -10:
         ys.append(y0)
+        cv2.line(img, (x0,y0), (x1,y1), (255, 0, 255), 2)
 
 ys.sort()
-dv = 30
-for i in range(len(ys) - 1):
-    if ys[i+1] - ys[i] < dv:
-        ys[i] = (ys[i] + ys[i+1]) /2
-        cv2.line(img2, (0,ys[i]), (img.shape[1],ys[i]), (255, 0, 255), 2)
+for i in range(0, 100):
+    consolidate(ys, 50, img, None)
+consolidate(ys, 50, img, (0, 255, 0))
+
+print ys
+ys = uniq(ys)
+print ys
 
 cv2.imwrite("gray.jpg", bw)
 cv2.imwrite("out.jpg", img)
-cv2.imwrite("out2.jpg", img2)
