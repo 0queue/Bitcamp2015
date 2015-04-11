62d28bef05c16553a483e4b64598a7f0b09ed160
Thomas Harris <teh019283@gmail.com>
2015-04-11 18:31:48 -0400

    Started server integration, Tester.py is MM+Slicer

--- ProofOfConcept/Tester.py
+++ ProofOfConcept/Tester.py
@@ -2,9 +2,10 @@
 
 from Slicer import Slicer
 from metamind.api import ClassificationData, ClassificationModel, set_api_key
+import cv2
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print ("Usage: Tester.py <key file> <picture>")
     exit(1)
 
@@ -13,5 +14,15 @@ with open(sys.argv[1], "r") as apikey:
 
 key = key.rstrip()
 
-print ("|" + key + "|")
 set_api_key(key)
+classifier = ClassificationModel(id=25007)
+print ("-----")
+
+s = Slicer(sys.argv[2], "out.jpg", True)
+s.create_slices(150)
+
+i = 0
+for slic in s.slics:
+    cv2.imwrite("CURRENT" + str(i) + ".jpg", slic)
+    print classifier.predict(["CURRENT" + str(i) + ".jpg"], input_type="files")
+    i += 1
